9ffd2de825eba247a784db6aa8bbe8eff177b6e9
yodatomato <yodatomato@live.jp>
2015-12-11 18:37:28 +0900

    コントローラーをネストした事による実行時エラーの対処その２。多分これで動きはOK

--- web/controllers/solution_controller.ex
+++ web/controllers/solution_controller.ex
@@ -7,36 +7,36 @@ defmodule Zohyohtanksgiving.SolutionController do
 
   def index(conn, %{"question_id" => question_id}) do
     solutions = Repo.all(Solution)
-    render(conn, "index.html", solutions: solutions, question_id: question_id)
+    render(conn, "index.html", question_id: question_id, solutions: solutions)
   end
 
   def new(conn, %{"question_id" => question_id}) do
     changeset = Solution.changeset(%Solution{})
-    render(conn, "new.html", changeset: changeset, question_id: question_id)
+    render(conn, "new.html", question_id: question_id, changeset: changeset)
   end
 
-  def create(conn, %{"solution" => solution_params, "question_id" => question_id}) do
+  def create(conn, %{"solution" => solution_params}) do
     changeset = Solution.changeset(%Solution{}, solution_params)
 
     case Repo.insert(changeset) do
-      {:ok, _solution} ->
+      {:ok, solution} ->
         conn
         |> put_flash(:info, "Solution created successfully.")
-        |> redirect(to: question_solution_path(conn, :index, question_id))
+        |> redirect(to: question_solution_path(conn, :index, solution.question_id))
       {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset, question_id: question_id)
+        render(conn, "new.html", question_id: solution_params.question_id, changeset: changeset)
     end
   end
 
   def show(conn, %{"id" => id}) do
     solution = Repo.get!(Solution, id)
-    render(conn, "show.html", solution: solution, question_id: solution.question_id)
+    render(conn, "show.html", question_id: solution.question_id, solution: solution)
   end
 
   def edit(conn, %{"id" => id, "question_id" => question_id}) do
     solution = Repo.get!(Solution, id)
     changeset = Solution.changeset(solution)
-    render(conn, "edit.html", solution: solution, changeset: changeset)
+    render(conn, "edit.html", question_id: question_id, solution: solution, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "solution" => solution_params}) do
@@ -47,9 +47,9 @@ defmodule Zohyohtanksgiving.SolutionController do
       {:ok, solution} ->
         conn
         |> put_flash(:info, "Solution updated successfully.")
-        |> redirect(to: question_solution_path(conn, :show, solution))
+        |> redirect(to: question_solution_path(conn, :show, solution.question_id, solution))
       {:error, changeset} ->
-        render(conn, "edit.html", solution: solution, changeset: changeset)
+        render(conn, "edit.html", question_id: solution.question_id, solution: solution, changeset: changeset)
     end
   end
 
@@ -62,6 +62,6 @@ defmodule Zohyohtanksgiving.SolutionController do
 
     conn
     |> put_flash(:info, "Solution deleted successfully.")
-    |> redirect(to: question_solution_path(conn, :index, question_id: question_id))
+    |> redirect(to: question_solution_path(conn, :index, question_id))
   end
 end
