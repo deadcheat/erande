6982d62ecefaeec46df74d722f6febf86aae5e5a
yodatomato <yodatomato@live.jp>
2015-12-16 19:07:09 +0900

    config修正漏れ

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,11 +3,11 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :phoenix_on_heroku, Zohyothanksgiving.Endpoint,
+config :zohyothanksgiving, Zohyothanksgiving.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :phoenix_on_heroku, Zohyothanksgiving.Repo,
+config :zohyothanksgiving, Zohyothanksgiving.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   size: 20 # The amount of database connections in the poo
