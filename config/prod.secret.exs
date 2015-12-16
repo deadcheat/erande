667aec32488c8e3ebae223e25f21221518ee57e3
yodatomato <yodatomato@live.jp>
2015-12-16 19:02:25 +0900

    herokuでビルドできるように設定を追加・修正

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -3,13 +3,11 @@ use Mix.Config
 # In this file, we keep production configuration that
 # you likely want to automate and keep it away from
 # your version control system.
-config :zohyothanksgiving, Zohyothanksgiving.Endpoint,
-  secret_key_base: "a3TMnYCR6/jj4GNIIRg8Le7tIBVe6APqEpnGB2au4NO0adYN0U7+tun137pBAiRN"
+config :phoenix_on_heroku, Zohyothanksgiving.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :zohyothanksgiving, Zohyothanksgiving.Repo,
+config :phoenix_on_heroku, Zohyothanksgiving.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "zohyothanksgiving_prod",
-  pool_size: 20
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the poo
