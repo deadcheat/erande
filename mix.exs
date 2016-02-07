90d6f3b15a0c6cfb27dc9176668c683054212367
yodatomato <yodatomato@live.jp>
2016-02-08 00:56:36 +0900

    fix  Elixir1.2必須にするとHerokuでDeploy出来ない

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Zohyothanksgiving.Mixfile do
   def project do
     [app: :zohyothanksgiving,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.1",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
